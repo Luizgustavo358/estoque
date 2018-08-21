73d3b2c07f791399729b00058042c8046ad768bc
Luiz <luiz.gustavo358@gmail.com>
2018-08-21 17:29:32 -0300

    mostrando produtos

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -4,21 +4,14 @@ namespace estoque\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
 
+use estoque\Produto;
+
 class ProdutoController extends Controller {
 
     public function lista() {
 
-        $html = '<h1>Listagem de produtos com Laravel.</h1>';
-        $html .= '<ul>';
-
         $produtos = DB::select('select * from produtos');
         
-        foreach($produtos as $p) {
-            $html .= '<li> <b>Nome:</b> '. $p->nome .',<br> <b>Descrição:</b> '. $p->descricao .'</li><br>';
-        }// end foreach
-
-        $html .= '</ul>';
-        
-        return $html;
+        return view('listagem')->with('produtos', $produtos);
     }// end lista()
 }// end class
